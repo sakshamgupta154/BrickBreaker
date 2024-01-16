53005ce6022be12a0ed162b4f7481862cfb5d661
Saksham <sakshamgupta154@gmail.com>
2024-01-16 20:59:39 +0530

    first commit

---
+++ src/brickbreaker/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brickbreaker;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Saksham
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        
+        Gameplay gamePlay = new Gameplay();
+        
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        obj.add(gamePlay);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
